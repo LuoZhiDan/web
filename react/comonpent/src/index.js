e1af77e26d027cfa0ad540c45c07d93000c7c906
zdluoa <17701023509@163.com>
2017-07-03 22:19:51 +0800

    create rctui module

--- react/comonpent/src/index.js
+++ react/comonpent/src/index.js
@@ -3,9 +3,3 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import Alert from './js/Alert';
-
-
-ReactDOM.render(
-  <Alert string="alert"/>,
-  document.getElementById('root')
-);
